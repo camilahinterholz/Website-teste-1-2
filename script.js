807a4ebf42a85d69e17a0f99abfdacc8eeaa39af
camilahinterholz <hinterholz.fotografia@gmail.com>
2025-04-25 15:28:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+  const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTopBtn.style.display = "block";
+    } else {
+    backToTopBtn.style.display = "none";
+    }
+  };
+
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+</script>
